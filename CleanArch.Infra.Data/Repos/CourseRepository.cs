ce8f0fc6e3b8ffb2f6cd51474543a03d43a85de0
Tamer Kamal <poseidonredsea@gmail.com>
2020-12-13 15:14:03 +0300

    Repo and Service implementation

---
+++ CleanArch.Infra.Data/Repos/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repos
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            this._context = context;
+        }
+        public  IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
